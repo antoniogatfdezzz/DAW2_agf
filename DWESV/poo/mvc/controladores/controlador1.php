583e5352224f0f6f4fedad0a1812fe0684fc7fdd
Antonio MacBook <antoniogatfernandez@iescastelar.com>
2025-11-06 19:42:50 +0100

    Nuevo tema

---
+++ DWESV/poo/mvc/controladores/controlador1.php
@@ -0,0 +1,16 @@
+<?php
+    /**
+     * controlador1.php
+     *  Responsabilidades:
+     *   - Recibir los datos de usuario
+     *   - Aplicar reglas de negocio
+     *   - Navegación entre vistas
+     */
+
+    class Controlador1 {
+        private $atributo1 = 'ATRIB1';
+
+        public function metodo1() {
+            echo "Ejecutando metodo1 de Controlador1<br>";
+        }
+    }
